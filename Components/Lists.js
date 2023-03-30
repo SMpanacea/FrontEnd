14d1c5b5087c1950365c9cb056b36393daa28d78
rhj7513 <81225901+rhj7513@users.noreply.github.com>
2023-03-30 11:48:30 +0900

    List Component 초기 디자인 완성
    약 목록 보여주는 Component 초기 디자인 완성함
    즐겨찾기한 약, 모든 약 보여줄 때 사용될 예정

--- Components/Lists.js
+++ Components/Lists.js
@@ -1,1 +1,52 @@
 // 약 정보 보여주는 리스트 컴포넌트
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const List = ({navigation}) => {
+
+  return (
+    // <TouchableOpacity onPress={()=>{navigation.navigate("MedicineDetail")}}>
+        <View style={styles.medibox}>
+            <Icon style={styles.mediicon} name="medkit" size={80} color="black" />
+            <View style={styles.medititletext}>
+              <Text style={styles.meditext}>약 이름</Text>
+              <Text>dfsdf</Text>
+            </View>
+        </View>          
+    // </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+medibox: {
+    flex:1,
+    flexDirection: 'row',
+    alignItems: "center",
+    borderWidth:1,
+    borderBottomColor: 'black',
+    marginBottom: '10%',
+    },
+    mediicon:{
+    borderWidth:1,
+    // height:'100%',
+    justifyContent: "center",
+    alignItems: "center",
+    },
+    medititletext:{
+        borderWidth:1,
+        borderColor:'blue',
+        width:'70%',
+    justifyContent: "center",
+    alignItems: "center",
+    },
+    meditext:{
+    borderWidth:1,
+    justifyContent: "center",
+    alignItems: "center",
+    },
+});
+
+
+export default List;
