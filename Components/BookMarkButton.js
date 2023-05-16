dfad78de3a9683c08d1d25f72475c91ba4cfc69a
rhj7513 <81225901+rhj7513@users.noreply.github.com>
2023-05-16 14:27:52 +0900

    BookMarkButton update
    BookMarkButton 즐겨찾기 수정 중,,,,

--- Components/BookMarkButton.js
+++ Components/BookMarkButton.js
@@ -18,12 +18,18 @@ function BookMarkButton ({medicinedetail, bookmark}){
 
   console.log("두근두근",bookmark) //배열 잘 가져와 근데 뭐가 문제임
   const [bookmarked, setBookmarked] = useState(true); //useState를 사용해서 즐겨찾기 했는지 안 했는지 알려줌 초기설정은 true임 
-  const foundMedicine = bookmark && bookmark.find((item) => item === medicinedetail.itemSeq);
+  const [foundMedicine,setFoundMedicine] = useState(false)
+ // const foundMedicine = bookmark && bookmark.find((item) => item === medicinedetail.itemSeq);
 
 
   console.log("도키도키", foundMedicine)
   const handleBookmark = () => { //bookmark핸들러
-  setBookmarked(!bookmarked); //bookmarked의 반대값을 setBookmarked에 저장해줌
+    const check = !bookmarked
+    setBookmarked(check); //bookmarked의 반대값을 setBookmarked에 저장해줌
+   
+    const checkFoundMedicine = check // && bookmark.find((item) => item === medicinedetail.itemSeq);
+    setFoundMedicine(checkFoundMedicine)
+    console.log("checkFoundMedicine", checkFoundMedicine)
 
 
 
@@ -47,7 +53,7 @@ function BookMarkButton ({medicinedetail, bookmark}){
     token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYWRtaW4yIiwiZXhwIjoxNjg0MDUxMDQxLCJpYXQiOjE2ODM0NDYyNDF9.BuFZu4A5VHSJHCqM89rpfWD2PzfKisITWLv9zexoefY"//걍 지정해줌
   };
 
-    if(foundMedicine){
+    if(checkFoundMedicine){
       axios.post(`${IP}/medicine/bookmarkoff`, res) // 별 색 있는 거 find했을 때 있으면 색 있는 거
       .then(response => {
         // 통신 성공 시 처리할 로직
