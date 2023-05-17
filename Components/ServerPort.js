5b18930fd7a9167e35fe98d80b24b9e22b75b0ea
rhj7513 <81225901+rhj7513@users.noreply.github.com>
2023-05-18 01:16:03 +0900

    ServerPort update
    ServerPort 수정해줌

--- Components/ServerPort.js
+++ Components/ServerPort.js
@@ -3,8 +3,8 @@ function ServerPort() {
     // return "http://10.20.38.124:8080"
     // return "http://192.168.0.6:8080"
     // return "http://172.16.39.21:8080"
-    // return "https://port-0-flask-test-p8xrq2mlfullttm.sel3.cloudtype.app/"
-    return "http://192.168.0.191:5000"
+    return "https://port-0-flask-test-p8xrq2mlfullttm.sel3.cloudtype.app/"
+    // return "http://172.16.36.35:5000"
 
 }
 
