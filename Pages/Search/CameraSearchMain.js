d9bd077db74f94537c8faf6cdbd354f3f2e24a9c
rhj7513 <81225901+rhj7513@users.noreply.github.com>
2023-05-20 06:13:48 +0900

    CameraSerchMain add
    CameraSerachMain은
    Main에서 알약 검색을 눌렀을 때 나오는 화면,,,

---
+++ Pages/Search/CameraSearchMain.js
@@ -0,0 +1,43 @@
+// main에서 알약 검색 누르면 보이는 화면
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, TouchableOpacity } from 'react-native';
+import { Text, Button } from 'react-native-paper';
+
+export default function Main({ navigation }) {
+
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Button
+                mode="outlined"
+                style={styles.down}
+                contentStyle={styles.button}
+                labelStyle={{ fontSize: 20 }}
+                onPress={console.log("카머라 버튼 눌림")}>카메라로 알약 검색</Button>
+            <Button
+                mode="outlined"
+                style={styles.down}
+                contentStyle={styles.button}
+                labelStyle={{ fontSize: 20 }}
+                onPress={console.log("얼역 버튼 눌림")}>이름으로 알약 검색</Button>
+            <Button
+                mode="outlined"
+                style={styles.down}
+                contentStyle={[styles.button]}
+                labelStyle={{ fontSize: 20 }}
+                onPress={console.log("카머라 버튼 눌림")}>카메라로 알약 검색</Button>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    button: {
+        height: 130, 
+        width: 300, 
+        alignItems: 'center', 
+        justifyContent: 'center',
+    },
+    down: {
+        marginBottom:60
+    }
+});
