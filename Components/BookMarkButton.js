bdd3ec5c4b76e424c513a2b59feb46a1cab29e93
rhj7513 <81225901+rhj7513@users.noreply.github.com>
2023-05-19 23:08:09 +0900

    BookMarkButton update
    BookMarkButton 토큰 만료된거 수정 후 에러 수정 중,,,

--- Components/BookMarkButton.js
+++ Components/BookMarkButton.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
-import bookmarkImage from '../assets/star.png';
-import bookmarkedImage from '../assets/binstar.png';
+import bookmarkImage from '../assets/star.png'; //색별
+import bookmarkedImage from '../assets/binstar.png'; //빈별
 import { useNavigation } from '@react-navigation/native';
 
 // 서버 포트
@@ -16,44 +16,44 @@ function BookMarkButton ({medicinedetail, bookmark}){
   
 
 
+//   console.log("두근두근",bookmark) //배열 잘 가져와 근데 뭐가 문제임
+//   const [bookmarked, setBookmarked] = useState(true); //useState를 사용해서 즐겨찾기 했는지 안 했는지 알려줌 초기설정은 true임 
+//   const [foundMedicine,setFoundMedicine] = useState()
+//  // const foundMedicine = bookmark && bookmark.find((item) => item === medicinedetail.itemSeq);
+
+
+//   console.log("도키도키", foundMedicine)
+//   const handleBookmark = () => { //bookmark핸들러
+//     const check = !bookmarked
+//     setBookmarked(check); //bookmarked의 반대값을 setBookmarked에 저장해줌
+   
+//     const checkFoundMedicine = check // && bookmark.find((item) => item === medicinedetail.itemSeq);
+//     setFoundMedicine(checkFoundMedicine)
+//     console.log("checkFoundMedicine", checkFoundMedicine)
+
+
+
+
+
+  
   console.log("두근두근",bookmark) //배열 잘 가져와 근데 뭐가 문제임
-  const [bookmarked, setBookmarked] = useState(true); //useState를 사용해서 즐겨찾기 했는지 안 했는지 알려줌 초기설정은 true임 
-  const [foundMedicine,setFoundMedicine] = useState(false)
- // const foundMedicine = bookmark && bookmark.find((item) => item === medicinedetail.itemSeq);
+  const [bookmarked, setBookmarked] = useState([]); //useState를 사용해서 즐겨찾기 했는지 안 했는지 알려줌 초기설정은 []임 
+  const foundMedicine = bookmark && bookmark.find((item) => item === medicinedetail.itemSeq);
 
 
   console.log("도키도키", foundMedicine)
   const handleBookmark = () => { //bookmark핸들러
-    const check = !bookmarked
-    setBookmarked(check); //bookmarked의 반대값을 setBookmarked에 저장해줌
-   
-    const checkFoundMedicine = check // && bookmark.find((item) => item === medicinedetail.itemSeq);
-    setFoundMedicine(checkFoundMedicine)
-    console.log("checkFoundMedicine", checkFoundMedicine)
-
-
-
-
-
-  
-  // console.log("두근두근",bookmark) //배열 잘 가져와 근데 뭐가 문제임
-  // const [bookmarked, setBookmarked] = useState([]); //useState를 사용해서 즐겨찾기 했는지 안 했는지 알려줌 초기설정은 true임 
-  // const foundMedicine = bookmark && bookmark.find((item) => item === medicinedetail.itemSeq);
-
-
-  // console.log("도키도키", foundMedicine)
-  // const handleBookmark = () => { //bookmark핸들러
-  // setBookmarked(!bookmarked); //bookmarked의 반대값을 setBookmarked에 저장해줌
-  
+  setBookmarked(!bookmarked); //bookmarked의 반대값을 setBookmarked에 저장해줌
+  console.log("넌 누구냐 진짜!!!!!!!!!",bookmarked)
   const res = {
     itemSeq: medicinedetail.itemSeq,
     itemName: medicinedetail.itemName, 
     itemImage: medicinedetail.itemImage, 
     updateDe:medicinedetail.updateDe,
-    token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYWRtaW4yIiwiZXhwIjoxNjg0MDUxMDQxLCJpYXQiOjE2ODM0NDYyNDF9.BuFZu4A5VHSJHCqM89rpfWD2PzfKisITWLv9zexoefY"//걍 지정해줌
+    token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibW9ua2V5MyIsImV4cCI6MTY4NTA5NTAxNCwiaWF0IjoxNjg0NDkwMjE0fQ.F9ZRcSS5Jb6zmFR6awLORFCsSxZvfBKCR1Mra8T00lQ"//걍 지정해줌
   };
 
-    if(checkFoundMedicine){
+    if(foundMedicine){
       axios.post(`${IP}/medicine/bookmarkoff`, res) // 별 색 있는 거 find했을 때 있으면 색 있는 거
       .then(response => {
         // 통신 성공 시 처리할 로직
@@ -81,6 +81,7 @@ function BookMarkButton ({medicinedetail, bookmark}){
     <TouchableOpacity onPress={handleBookmark}> 
       <View style={styles.bookmarkbutton}>
         <Image source={bookmarked ?  bookmarkImage : bookmarkedImage} style={styles.image} /> 
+        {/*bookmarked에 값이 있으면 색별, 아니면 빈별 뜨게 해줌*/}
       </View>
     </TouchableOpacity>
   );
