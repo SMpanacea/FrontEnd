dc54aab506217ee85997ca5c4801c23cb6775895
smreo3839 <smreo3839@gmail.com>
2023-05-28 11:01:43 +0900

    port 바꿈
    port 바꿈

--- Components/ServerPort.js
+++ Components/ServerPort.js
@@ -1,6 +1,6 @@
 function ServerPort() {
     // return "https://port-0-flask-test-p8xrq2mlfullttm.sel3.cloudtype.app/"
-    return "http://172.16.32.28:5000"
+    return "http://172.16.36.14:5000"
 }
 
 export default ServerPort;
