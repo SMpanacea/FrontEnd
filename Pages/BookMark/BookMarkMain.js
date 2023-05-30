f2883588dcb4b102a117f7d40fd1730539027dbd
rhj7513 <81225901+rhj7513@users.noreply.github.com>
2023-05-30 16:42:59 +0900

    BookMarkMain update
    BookMarkMain에서 목록 보여주고 디테일 들어가서 즐찾 해제했을 때 다시 목록으로 돌아가면 최신화하지 못하는 에러 해결

--- Pages/BookMark/BookMarkMain.js
+++ Pages/BookMark/BookMarkMain.js
@@ -30,43 +30,32 @@ function BookMarkMain({navigation}) {
   const [tokens, setTokens] = React.useState("");
 
   const [bookmark, setBookmark] = React.useState([]);//bookmark 리스트 있는지 확인
-  React.useEffect(async () => {
+
+  React.useEffect(()=> {
+    console.log("북마크 바뀜")
+    BookmarkListall()
+  },[bookmark])
+
+  const BookmarkListall = async () => {
     const getToken = await AsyncStorage.getItem('token');
     console.log("bookmark token: ", getToken);
-    
-    const BookmarkListall = async () => {
-      try {
-        const res = await axios.post(`${IP}/medicine/bookmarkall`, {
-          token: getToken
-        });
-        console.log("bookmark res.data: ", res.data);
-        setMedicinedata(res.data);
-        console.log("bookmarkall에서 가져온 놈임", medicinedata);
-        setBookmarkmedicine(res.data || []); // bookmark가 null인 경우 빈 배열로 처리
-        setTokens(getToken);
-        console.log("하이류ㅜ",tokens)
-      } catch (e) {
-        console.log("즐겨찾기 목록 못 가져옴,,,", e);
-      }
-    };
+    try {
+      const res = await axios.post(`${IP}/medicine/bookmarkall`, {
+        token: getToken
+      });
+      console.log("bookmark res.data: ", res.data);
+      setMedicinedata(res.data);
+      console.log("bookmarkall에서 가져온 놈임", medicinedata);
+      setBookmarkmedicine(res.data || []); // bookmark가 null인 경우 빈 배열로 처리
+      setTokens(getToken);
+      console.log("하이류ㅜ",tokens)
+    } catch (e) {
+      console.log("즐겨찾기 목록 못 가져옴,,,", e);
+    }
+  };
 
-  React.useLayoutEffect(() => {
-    navigation.setOptions({
-      headerLeft: () => (
-          <TouchableRipple onPress={() => navigation.goBack()} accessibilityLabel='뒤로가기'>
-              <Image source={require('../../assets/left.png')} style={{ width: 30, height: 30, marginLeft: 10 }} />
-          </TouchableRipple>
-      ),
-      headerTitle: "바코드 인식",
-      headerStyle: {
-        elevation: 10, // 안드로이드 그림자 효과
-        shadowOpacity: 0.5, // iOS 그림자 효과
-        shadowColor: 'black', // 그림자 색상 설정
-        shadowOffset: { width: 0, height: 2 }, // 그림자 오프셋 설정
-        shadowRadius: 4, // 그림자 반경 설정
-      },
-    });
-  }, [])
+  React.useEffect(async () => {
+    
 
     if (getToken === null) {
       // getToken이 null일 때 빈 배열을 처리
@@ -116,6 +105,7 @@ function BookMarkMain({navigation}) {
             medicinedata={medicinedata} 
             bookmark={bookmark} //bookmark list넘겨줌
             token={tokens} //token bookmark로 넘겨줌
+            setBookmark={setBookmark}
             setBookmarkmedicine={setBookmarkmedicine} //bookmark list를 변경하는 함수 넘겨줌
             onPress={(medicinename, bookmark) => {
               navigation.navigate('Detail', { medicinename, bookmark })
